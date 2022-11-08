1ff27ae04a20079d11caade3ded5d5837221b573
lttnml1 <littonml1@gmail.com>
2022-11-07 16:04:55 -0800

    hybrid

---
+++ ce_python/point_pair.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+class PointPair:
+    def __init__(self, pt1: 'PathPoint', pt2: 'PathPoint') -> None:
+        self.pt1: 'PathPoint' = pt1
+        self.pt2: 'PathPoint' = pt2
